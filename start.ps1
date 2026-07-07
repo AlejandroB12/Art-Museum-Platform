6ed5db4be06feb703114700702ac0e1aa3fdd2b1
Kelvys Concepcion ✨ <kelvys.10cena@gmail.com>
2026-07-07 15:02:45 -0400

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -19,7 +19,7 @@ const results = [];
     // MySQL
     try {
         const mysql = require('mysql2');
-        const db = mysql.createConnection({host:'localhost',user:'root',password:process.env.DB_PASSWORD_MYSQL,database:process.env.DB_NAME_MYSQL});
+        const db = mysql.createConnection({host:process.env.DB_HOST,user:process.env.DB_USER,password:process.env.DB_PASSWORD_MYSQL,database:process.env.DB_NAME_MYSQL});
         await new Promise((resolve,reject) => db.connect(err=>err?reject(err):resolve()));
         db.end();
         results.push('  [MySQL]     OK');
